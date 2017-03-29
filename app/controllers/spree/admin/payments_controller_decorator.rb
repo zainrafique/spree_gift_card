8268f7df27a3723c770a065a2f5c8ad611c8755e
Anurag Bhardwaj <anuragbhardwaj.mait@gmail.com>
2017-03-29 16:34:04 +0530

    made order.outstanding_balance_after_applied_store_credit public as change requested

--- app/controllers/spree/admin/payments_controller_decorator.rb
+++ app/controllers/spree/admin/payments_controller_decorator.rb
@@ -7,7 +7,7 @@ Spree::Admin::PaymentsController.class_eval do
         @order.payments.gift_cards.checkout.map(&:invalidate!)
         @payment = @order.payments.build
         @payment.source = gift_card
-        @payment.amount = [gift_card.amount_remaining, @order.send(:outstanding_balance_after_applied_store_credit)].min
+        @payment.amount = [gift_card.amount_remaining, @order.outstanding_balance_after_applied_store_credit].min
         @payment.payment_method = gift_card_payment_method
       end
     end
@@ -26,6 +26,6 @@ Spree::Admin::PaymentsController.class_eval do
   end
 
   def gift_card_code
-    @gift_card_code  ||= (params[:payment_source][params[:payment][:payment_method_id]])[:code]
+    @gift_card_code ||= (params[:payment_source][params[:payment][:payment_method_id]])[:code]
   end
 end
