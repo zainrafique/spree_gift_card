3e4af4af65ea5e66993b1554bf4d63468ac5ae0f
Himanshu <himanshumishra3@gmail.com>
2018-04-04 19:04:44 +0530

    upgraded to 3.2 3.3 3.4

--- db/migrate/20180404061553_add_enable_field_to_gift_card.rb
+++ db/migrate/20180404061553_add_enable_field_to_gift_card.rb
@@ -1,4 +1,4 @@
-class AddEnableFieldToGiftCard < ActiveRecord::Migration
+class AddEnableFieldToGiftCard < ActiveRecord::Migration[4.2]
   def change
     add_column :spree_gift_cards, :enabled, :boolean, default: false
   end
